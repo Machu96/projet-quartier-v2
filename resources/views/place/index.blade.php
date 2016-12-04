76926cc9c6e2d0e83f8f07bd981406367b546a54
RochezBaptiste <rochez.baptiste@gmail.com>
2016-12-04 11:17:28 +0100

    Ajout boutton open/close + correction css page Place

--- resources/views/place/index.blade.php
+++ resources/views/place/index.blade.php
@@ -28,7 +28,8 @@
             <div class="map"><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11133.988129538877!2d4.8169921898783965!3d45.76122843568518!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4ebaaef7e7f5f%3A0x2d9cc97768f72426!2sVieux+Lyon%2C+69005+Lyon%2C+France!5e0!3m2!1sfr!2sfr!4v1478164165338" width="100%" height="100%" frameborder="0" style="border:0" allowfullscreen></iframe></div>
         </div>
     </div>
-    <div style="text-align: center;"><a onclick="openpanel(0)">+ info</a></div>
+    <div class="openbutton" style="text-align: center;"><a onclick="openpanel(0)">+ info</a></div>
+    <div class="closebutton" style="text-align: center; display: none;"><a onclick="closepanel(0)">close</a></div>
 
 </div>
 @endsection
@@ -47,6 +48,25 @@
         var map = document.getElementsByClassName('container-map');
         map[positionelement].style.display='initial';
         map[positionelement].style.visibility='initial';
+        var buttonopen = document.getElementsByClassName('openbutton');
+        buttonopen[positionelement].style.display='none';
+        var buttonclose = document.getElementsByClassName('closebutton');
+        buttonclose[positionelement].style.display='block';
+    }
+    function closepanel(positionelement){
+        var image = document.getElementsByClassName('wrap-picture-inside');
+        image[positionelement].style.height='200px';
+        var text = document.getElementsByClassName('wrap-text');
+        text[positionelement].style.height='140px';
+        var textinner = document.getElementsByClassName('wrap-text-inner');
+        textinner[positionelement].style.width='100%';
+        var map = document.getElementsByClassName('container-map');
+        map[positionelement].style.display='none';
+        map[positionelement].style.visibility='hidden';
+        var buttonopen = document.getElementsByClassName('openbutton');
+        buttonopen[positionelement].style.display='block';
+        var buttonclose = document.getElementsByClassName('closebutton');
+        buttonclose[positionelement].style.display='none';
     }
 
 </script>
