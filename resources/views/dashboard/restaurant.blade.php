3d38e2d845df9b7b3ca579beb24647f74d49f56d
RochezBaptiste <rochez.baptiste@gmail.com>
2016-12-07 13:29:07 +0100

    Design admin panel (#26)

--- resources/views/dashboard/restaurant.blade.php
+++ resources/views/dashboard/restaurant.blade.php
@@ -11,32 +11,32 @@
 
     {!! Form::open(['url' => url('restaurants'), 'method' => 'post', 'files' => true]) !!}
 
-    <div class="input-group">
+    <div class="form-group">
         <label for="name">Nom</label>
         <input name="name" id="name" class="form-control" type="text">
     </div>
 
-    <div class="input-group">
+    <div class="form-group">
         <label for="description">Description</label>
         <textarea class="form-control" name="description" id="description" cols="30" rows="10"></textarea>
     </div>
 
-    <div class="input-group">
+    <div class="form-group">
         <label for="address">Adresse</label>
         <input name="address" id="address" class="form-control" type="text">
     </div>
 
-    <div class="input-group">
+    <div class="form-group">
         <label for="latitude">Latitude</label>
         <input name="latitude" id="latitude" class="form-control" type="number">
     </div>
 
-    <div class="input-group">
+    <div class="form-group">
         <label for="longitude">Longitude</label>
         <input name="longitude" id="longitude" class="form-control" type="number">
     </div>
 
-    <div class="input-group">
+    <div class="form-group">
         <label for="image">Longitude</label>
         <input class="form-control" name="image" id="image" type="file">
     </div>
