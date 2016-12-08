1a4916ed141668c92dd8b4e09760b477bb158d29
thomas <thomas.vincentbtsnir@gmail.com>
2016-12-08 18:08:05 +0100

    Ajout du texte un peu d'histoire sur homepage + ajout de la trad finale

--- resources/lang/en/homepage.php
+++ resources/lang/en/homepage.php
@@ -11,11 +11,12 @@ return [
     'titre2' => 'the oldest district of Lyon',
     'titre3' => 'Organize your own visit',
     'histoire' => "A little bit of history...",
-    'histoire-text' => '????',
-
+    'histoire-text' => 'Old lyon is is the medieval and rennaissance district of Lyon. It is located next to the Saône river. This is one of the largest areas for this period has remained intact to this day. It is a UNESCO World heritage site.There are traboules, the Saint Jean district, the slope of the red cross, Fourvière and the Saint Georges quarter. The whole of Vieux Lyon extends over 30 ha, has 500 buildings, 3,000 dwellings and 7,000 inhabitants. ',
+    'histoire-text2' => 'Saint-Jean is one of the three districts that make up the Old Lyon with St Paul and St Georges. Central point of the programs of restoration, it is the obligatory passage of the tourists and the Lyonnais. Rue Saint Jean is made up of buildings built during the Renaissance. It is an unmissable place of Lyon, it is very alive. There is also a fine artisanal offer: luthiers, puppet makers and small very pleasant markets.',
     'datavision1' => 'Cultural monuments',
     'datavision2' => 'Traboules',
     'datavision3' => 'Of the inhabitants are happy in the district',
+
 
 
 ];
--- resources/lang/fr/homepage.php
+++ resources/lang/fr/homepage.php
@@ -12,7 +12,8 @@ return [
     'titre2' => 'le plus vieux quartier de Lyon',
     'titre3' => 'Organisez votre propre visite du quartier',
     'histoire' => "Un peu d'histoire...",
-    'histoire-text' => '????',
+    'histoire-text' => 'Le vieux Lyon est le quartier médiéval et renaissance de Lyon. Il est situé à côté da la Saône. C’est l’un des quartiers les plus vastes concernant cette période qui est resté intact jusqu’à nos jours. Il est classé au patrimoine mondial de l’UNESCO. Il y a des traboules, le quartier Saint Jean, la pente de la croix rousse, Fourvière et le quartier Saint Georges. L’ensemble du Vieux Lyon s’étend sur 30 ha, compte 500 immeubles, 3 000 logements et 7 000 habitants. ',
+    'histoire-text2' => 'Saint-Jean est l’un des 3 quartiers qui constitue le Vieux Lyon avec St Paul et St Georges.Point central des programmes de restauration, c’est le passage obligé des touristes et des Lyonnais. La rue Saint jean est constitué d’immeubles construits à la renaissance. C’est un endroit incontournable de Lyon, il est très vivant. Il y a aussi une belle offre artisanale : des luthiers, des fabricants de marionnettes et des petits marchés très agréables.',
 
     'datavision1' => 'Monument culturels',
     'datavision2' => 'Traboules',
